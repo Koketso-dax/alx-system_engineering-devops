5439448d589320dd5b78900011b39291e0f4db94
Koketso-dax <lanchaster.k@gmail.com>
2024-04-22 22:10:51 +0200

    Employee id fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,24 +5,29 @@ module to write TODO's
 import requests
 import sys
 
+
 def get_employee_todo_progress(employee_id):
     """
     Get employee's stored items
     """
-    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    strUserResponse = "https://jsonplaceholder.typicode.com/users/{}"
+    user_response = requests.get(strUserResponse.format(employee_id))
     user_data = user_response.json()
-    employee_name = user_data.get('name', 'Unknown')
+    employee_name = user_data.get('name')
 
-    todos_response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+    strTodoResponse = "https://jsonplaceholder.typicode.com/todos?userId={}"
+    todos_response = requests.get(strTodoResponse.format(employee_id))
     todos_data = todos_response.json()
 
     completed_tasks = [todo for todo in todos_data if todo['completed']]
     num_completed_tasks = len(completed_tasks)
     total_num_tasks = len(todos_data)
 
-    print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_num_tasks}):")
+    print("Employee {} is done with tasks ({}/{}): "
+          .format(employee_name, num_completed_tasks, total_num_tasks))
     for task in completed_tasks:
         print(f"\t{task['title']}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2 or not sys.argv[1].isdigit():
