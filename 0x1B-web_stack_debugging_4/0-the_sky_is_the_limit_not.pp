7e60984e2a1957c07ce61682ce68eb51295e56cb
Koketso-dax <lanchaster.k@gmail.com>
2024-05-17 11:57:09 +0200

    optimize nginx with custom ERB

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -6,8 +6,51 @@ exec { 'fix--for-nginx':
   path    => '/usr/local/bin/:/bin/'
 }
 
+# Create the nginx.conf.erb template
+file { '/etc/puppetlabs/code/environments/production/modules/nginx/templates/nginx.conf.erb':
+  ensure  => file,
+  content => '
+user www-data;
+worker_processes auto;
+pid /run/nginx.pid;
+
+events {
+    worker_connections 1024;
+    multi_accept on;
+}
+
+http {
+    include /etc/nginx/mime.types;
+    default_type application/octet-stream;
+
+    access_log /var/log/nginx/access.log;
+    error_log /var/log/nginx/error.log;
+
+    sendfile on;
+    tcp_nopush on;
+    tcp_nodelay on;
+    keepalive_timeout 65;
+    types_hash_max_size 2048;
+
+    gzip on;
+    gzip_disable "msie6";
+
+    include /etc/nginx/conf.d/*.conf;
+    include /etc/nginx/sites-enabled/*;
+}
+',
+  mode    => '0644',
+}
+
+# Update Nginx configuration
+file { '/etc/nginx/nginx.conf':
+  ensure  => file,
+  content => template('nginx/nginx.conf.erb'),
+  notify  => Exec['nginx-restart'],
+}
+
 # Restart Nginx
 exec { 'nginx-restart':
-  command => '/usr/sbin/service nginx restart',
-  path    => ['/usr/sbin', '/sbin', '/bin', '/usr/bin'],
+  command => 'service nginx restart',
+  require => File['/etc/nginx/nginx.conf'],
 }
