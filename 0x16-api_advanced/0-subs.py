ed30b3f3e433b417227e5183191a5f24133b57a7
Koketso-dax <lanchaster.k@gmail.com>
2024-08-13 15:19:29 +0200

    disallow redirects on requests for top_ten

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,21 +8,26 @@ import requests.exceptions
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
+    """
+    Returns the number of subscribers for a given subreddit.
+    """
     if subreddit is None or not isinstance(subreddit, str):
         return 0
-    user_agent = '0x16-api_advanced:project:v1.0.0 (by /u/koketso-dax)'
+
+    user_agent = {'User-Agent':'0x16-api_advanced:project/u/koketso-dax)'}
+
     # Make the GET request with no redirects allowed
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+
     try:
-        r = requests.get(url, headers={'User-Agent': user_agent},
+        r = requests.get(url, headers=user_agent,
                          allow_redirects=False)
-        # Check if the request was successful and parse JSON
+
         if r.status_code == 200:
             subs = r.json().get("data", {}).get("subscribers", 0)
             return subs
         else:
-            # Handle cases where the status code indicates an error
+            # Handle cases for status code errors
             return 0
     except requests.exceptions.RequestException:
         return 0
