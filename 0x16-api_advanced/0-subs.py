49e6ace3d514c338bb4c3ad6444c9b5066540cda
Koketso-dax <lanchaster.k@gmail.com>
2024-08-13 15:48:40 +0200

    simplify 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,18 +11,13 @@ def number_of_subscribers(subreddit):
     """
     Returns the number of subscribers for a given subreddit.
     """
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
 
-    user_agent = {'User-Agent': '0x16-api_advanced:project/u/koketso-dax)'}
-
-    # Make the GET request with no redirects allowed
+    user_agent = {'User-Agent': '0x16-api_advanced:project/u/koketso-dax/0.1.0'}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
     r = requests.get(url, headers=user_agent, allow_redirects=False)
-    if r.status_code == 404:
-        return 0
-
-    else:
-        subs = r.json().get("data").get("subscribers")
+    
+    if r.status_code == 200:
+        subs = r.json().get('data').get('subscribers')
         return subs
+    else:
+        return 0
