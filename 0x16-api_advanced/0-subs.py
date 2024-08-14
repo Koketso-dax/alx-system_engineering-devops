a5fd9a5a14fd56c375f4b63db9c07e0812e6842a
Koketso-dax <lanchaster.k@gmail.com>
2024-08-14 16:01:13 +0200

    updated user agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,10 +12,10 @@ def number_of_subscribers(subreddit):
     Returns the number of subscribers for a given subreddit.
     """
 
-    user_agent = {'User-Agent': '0x16-api_advanced:v0.0.1 (by /u/NearbyProposal7738)'}
+    user_agent = {'User-Agent': '0x16-api_advanced:v0.0.1 \
+                  (by /u/NearbyProposal7738)'}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    r = requests.get(url, headers=user_agent, allow_redirects=False)
-    
+    r = requests.get(url, headers=user_agent, allow_redirects=False)    
     if r.status_code == 200:
         subs = r.json().get('data').get('subscribers')
         return subs
